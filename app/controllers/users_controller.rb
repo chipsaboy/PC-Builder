e7f4091da7a7e5439758a789c6aad19dfc388bb4
Nathan Campbell <nathancampbell@outlook.com>
2017-07-14 13:27:44 -0500

    added post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,4 +27,14 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/'
+    else
+      redirect to '/builds'
+    end
+  end
+
 end
