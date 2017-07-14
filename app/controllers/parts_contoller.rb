09682d86f0c72a108ea71593f25e70ee863b2247
Nathan Campbell <nathancampbell@outlook.com>
2017-07-14 13:47:03 -0500

    added post new part to parts controller

--- app/controllers/parts_contoller.rb
+++ app/controllers/parts_contoller.rb
@@ -10,4 +10,18 @@ class PartsController < ApplicationController
     end
   end
 
+  post '/builds/:id' do
+    if params.values.include?("")
+      @build = Build.find(params[:id])
+      flash[:message] = "Please fill out all fields."
+      erb :'parts/new'
+    else
+      @build = Build.find(params[:id])
+      @part = Part.new(name: params[:name], price: params[:price])
+      @part.save
+      @build.parts << @part
+      redirect to "/builds/#{@build.id}"
+    end
+  end
+
 end
